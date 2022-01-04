eee7efb1233510acae09d540d8a7abc8e627db69
Ningthangom <ningthangom@gmail.com>
2022-01-04 14:10:10 +1030

    changed socket port

--- src/connectBackend/socketConnect.js
+++ src/connectBackend/socketConnect.js
@@ -2,5 +2,5 @@ import React from 'react';
 import * as socketio from "socket.io-client";
 
 
-export const socket = socketio.connect('http://localhost:8000');
+export const socket = socketio.connect(process.env.REACT_APP_API_SOCKET);
 export const SocketContext = React.createContext();
